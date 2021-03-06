55617e20338d660ca4e2718abf1446f637f16244
Sven Gehring <cbrxde@gmail.com>
2021-03-06 14:55:08 +0100

    bump

--- TIAE5-DB-Mini/CaseStudyContext.cs
+++ TIAE5-DB-Mini/CaseStudyContext.cs
@@ -12,5 +12,11 @@ namespace TIAE5_DB_Mini.Models
         public DbSet<Grundbuchamt> grundbuchamts { get; set; }
         public DbSet<Mitarbeiter> mitarbeiters { get; set; }
         public DbSet<Objekt> objekts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Gefaehrdung>(entity => entity.HasCheckConstraint("CK_Gefaehrdung_Stufe_Min", "[gefaehrdungsstufe] > 0"));
+            modelBuilder.Entity<Gefaehrdung>(entity => entity.HasCheckConstraint("CK_Gefaehrdung_Stufe_Max", "[gefaehrdungsstufe] <= 0"));
+        }
     }
 }
