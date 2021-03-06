26f4d1440524df8dd2b7122f84f3ddb3d333c5f4
André Glatzl <andre@alpinesoft.ch>
2021-03-06 12:51:14 +0100

    Created Models

---
+++ TIAE5-DB-Mini/Models/Grundbuchamt.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TIAE5_DB_Mini.Models
+{
+    public class Grundbuchamt
+    {
+
+    }
+}
---
+++ TIAE5-DB-Mini/Models/Mitarbeiter.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TIAE5_DB_Mini.Models
+{
+    public class Mitarbeiter
+    {
+    }
+}
