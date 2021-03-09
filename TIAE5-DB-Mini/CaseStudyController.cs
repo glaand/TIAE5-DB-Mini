9e3fe90456d10a2132d9a658f2d0d10f48ba404b
Sven Gehring <cbrxde@gmail.com>
2021-03-09 16:01:26 +0100

    use db context based on passed user

--- TIAE5-DB-Mini/CaseStudyController.cs
+++ TIAE5-DB-Mini/CaseStudyController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
 using TIAE5_DB_Mini.Models;
 
 namespace TIAE5_DB_Mini.Controllers {
@@ -17,7 +18,26 @@ namespace TIAE5_DB_Mini.Controllers {
 
         protected CaseStudyGenericContext GetContext()
         {
-          return this._contextExternal;
+            string authorization = Request.Headers["Authorization"];
+            try
+            {
+               string authInfo = this.decodeHeader(authorization.Replace("Basic ", ""));
+
+               // TODO: This should be rewritten into an implementation, where the auth information
+               // is stored in the application environment, not pushed to version control and is
+               // not the same as the database credentials. For the sake of this demonstration,
+               // security is not a concern.
+               if (authInfo == "interner:password") return this._contextInternal;
+               if (authInfo == "externer:password") return this._contextExternal;
+            } catch {}
+
+            return this._contextExternal;
+        }
+
+        private string decodeHeader(string base64)
+        {
+            byte[] data = Convert.FromBase64String(base64);
+            return System.Text.Encoding.UTF8.GetString(data);
         }
     }
 }
